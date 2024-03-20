c1b1c068397d2501f1853e82752ae323af7c6916
swko630 <kosw630@naver.com>
2024-03-20 17:16:04 +0900

    20240320

---
+++ 06_aop/src/main/java/com/gdu/prj06/service/ContactService.java
@@ -0,0 +1,19 @@
+package com.gdu.prj06.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gdu.prj06.dto.ContactDto;
+
+public interface ContactService {
+  // service - controller - view, service 에서 view로 한번에 보낸다.
+  void registerContact(HttpServletRequest request, HttpServletResponse response);
+  void modifyContact(HttpServletRequest request, HttpServletResponse response);
+  void removeContact(HttpServletRequest request, HttpServletResponse response);
+  List<ContactDto> getContactList();
+  ContactDto getContactByNo(int contactNo);
+  void txTest();
+  
+}
